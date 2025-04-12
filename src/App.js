9b4ede4df23099d2b2d34a98f088c861dbe27c91
Rose Momanyi <rosebmomanyi@gmail.com>
2025-04-12 13:42:49 +0300

    Modified app.js

--- src/App.js
+++ src/App.js
@@ -2,23 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [expenses, setExpenses] = useState([]);
+  const [filterYear, setFilterYear] = useState('2023');
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => [expense, ...prevExpenses]);
+  };
+
+  const filteredExpenses = expenses.filter(
+    (expense) => expense.date.getFullYear().toString() === filterYear
+  );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Expense Tracker</h1>
+    <ExpenseForm onAddExpense={addExpenseHandler} />
+    <ExpenseFilter selectedYear={filterYear} onChangeYear={setFilterYear} />
+    <ExpenseList expenses={filteredExpenses} />
+  </div>
+   
   );
 }
 
